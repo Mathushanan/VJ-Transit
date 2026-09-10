311c08e9dfe0440d8257698563e4ac03af4e2890
Mathushanan-Mahendrarajah <10898816@students.plymouth.ac.uk>
2026-09-10 18:49:24 +0530

    navbar implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,36 @@
 import { useState } from "react";
-import heroImg from "./assets/hero.png";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
+import About from "./pages/About";
+import Services from "./pages/Services";
+import Fleet from "./pages/Fleet";
+import ServiceAreas from "./pages/ServiceAreas";
+import Quote from "./pages/Quote";
+import Contact from "./pages/Contact";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>App</div>
-    </>
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/about" element={<About />} />
+
+        <Route path="/services" element={<Services />} />
+
+        <Route path="/fleet" element={<Fleet />} />
+
+        <Route path="/service-areas" element={<ServiceAreas />} />
+
+        <Route path="/quote" element={<Quote />} />
+
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,109 @@
-import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
+import logo from "../assets/logo.png";
 
-const Navbar = () => {
-  return <div>Navbar</div>;
-};
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg vj-navbar">
+      <div className="container">
+        {/* Logo */}
+        <NavLink className="navbar-brand vj-logo" to="/">
+          <img src={logo} alt="VJ Transit" className="vj-logo-img" />
+        </NavLink>
+
+        {/* Mobile Menu Button */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#vjNavbar"
+          aria-controls="vjNavbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <i className="bi bi-list"></i>
+        </button>
+
+        {/* Navigation */}
+        <div className="collapse navbar-collapse" id="vjNavbar">
+          <ul className="navbar-nav mx-auto align-items-lg-center">
+            <li className="nav-item">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+                to="/"
+                end
+              >
+                Home
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+                to="/about"
+              >
+                About
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+                to="/services"
+              >
+                Services
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+                to="/fleet"
+              >
+                Fleet
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+                to="/service-areas"
+              >
+                Service Areas
+              </NavLink>
+            </li>
+
+            <li className="nav-item">
+              <NavLink
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link"
+                }
+                to="/contact"
+              >
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+
+          {/* Quote Button */}
+          <NavLink className="btn quote-btn" to="/quote">
+            Get a Quote
+            <i className="bi bi-file-earmark-text ms-2"></i>
+          </NavLink>
+        </div>
+      </div>
+    </nav>
+  );
+}
 
 export default Navbar;
